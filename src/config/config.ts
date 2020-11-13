ef1b92eaf340a8fecd8281f203f8b548eeef656c
Chifuyu <chifuyu@nate.com>
2020-11-13 10:42:23 +0900

    change: front baseUrl

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,1 +1,1 @@
-export const baseUrl = `http://localhost:3010`;
+export const baseUrl = `http://3.35.216.163/`;
--- src/saga/index.ts
+++ src/saga/index.ts
@@ -3,8 +3,9 @@ import axios from 'axios';
 import loginSaga from './login';
 import userSaga from './user';
 import postSaga from './post';
+import { baseUrl } from '../config/config';
 
-axios.defaults.baseURL = 'http://localhost:3010';
+axios.defaults.baseURL = baseUrl;
 axios.defaults.withCredentials = true;
 
 export default function* rootSaga() {
